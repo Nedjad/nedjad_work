2c181a195f6bc8c338f32997539f3567f0be5ff9
Nedjad <mesh.nedzhad@gmail.com>
2021-02-17 13:14:00 +0300

    Домашнее задание по первой лекции.

---
+++ intro to csharp/home_work1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_work1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите ваше имя! \n");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("\nПривет, {0}! \nаТекущая дата и время: {1}", userName, DateTime.Now.ToString("dd.MM.yyyy HH.mm.ss"));
+            Console.ReadKey();
+           
+        }
+    }
+}
