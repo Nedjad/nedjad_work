3cebc54280a07a11ea45d49e16dfd98d2fe74287
Nedjad <mesh.nedzhad@gmail.com>
2021-02-19 10:42:36 +0300

    Домашняя работа 2.1

---
+++ intro to csharp/home_work2/home_work 2.1/averageTemp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace averageTemp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет! Давай проверим среднюю температуру за сегодняшний день!");
+            Console.WriteLine("Введи минимальную температуру!");
+
+            double minTemp = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("А теперь введи максимальную температуру");
+
+            double maxTemp = double.Parse(Console.ReadLine());
+
+            double result = (maxTemp + minTemp) / 2;
+
+            DateTime dt = new DateTime();
+            dt = DateTime.Now;
+            
+
+
+            Console.WriteLine($"Средняя температура за {dt} равна {result}");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
