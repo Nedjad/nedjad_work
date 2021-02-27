6cb6d9196d7872611cd258aab805e4c547ebf538
Nedjad <mesh.nedzhad@gmail.com>
2021-02-27 20:00:31 +0300

    Задача 3.3

---
+++ intro to csharp/homeWork_3.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваш текст.");
+
+            string userText = Console.ReadLine();
+
+            Console.WriteLine(userText.Reverse().ToArray());
+            Console.ReadLine();
+
+
+
+            
+        }
+    }
+}
