34f7e2c9e313bfd2b7055d295d3d40e98b0fbf82
Nedjad <mesh.nedzhad@gmail.com>
2021-03-07 00:31:06 +0300

    4.1 Вывод ФИО введённых пользователем.

---
+++ intro to csharp/homeWork_4.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace homeWork_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            (string firstName, string LastName, string patronymic) = GetFullName();
+            Console.ReadLine();
+            
+            
+        }
+        public static (string firstName, string lastName, string patronymic) GetFullName()
+        {
+            string firstName = "";
+            string lastName = "";
+            string patronymic = "";
+            
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Введите имя.");
+                firstName = Console.ReadLine();
+
+                Console.WriteLine("Введите фамилию.");
+                lastName = Console.ReadLine();
+
+                Console.WriteLine("Введите отчество.");
+                patronymic = Console.ReadLine();
+                
+                Console.WriteLine($"{lastName} {firstName} {patronymic}");
+
+            }
+            
+            
+            return (firstName, lastName, patronymic);
+            
+
+            
+            
+            
+            
+            
+
+            
+            
+
+            
+        }
+    }
+}
