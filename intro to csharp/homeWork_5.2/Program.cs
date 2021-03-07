8319ba4716807b98cb489e959bac1569fc7ca8ef
Nedjad <mesh.nedzhad@gmail.com>
2021-03-08 00:17:11 +0300

    Задача 5.2 Вывод текущей даты в файл.

---
+++ intro to csharp/homeWork_5.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace homeWork_5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              
+            File.AppendAllText(@"C:\Users\meshn\Desktop\startup.txt", DateTime.Now.ToString());
+        }
+    }
+}
