41cf9eb789c06b2a80ba6e556ea8b3261a11190f
Nedjad <mesh.nedzhad@gmail.com>
2021-03-08 00:07:59 +0300

    Задача 5.1 Запись текста в файл.

---
+++ intro to csharp/homeWork_5.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace homeWork_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string word = Console.ReadLine();
+
+            
+            File.AppendAllText(@"C:\Users\meshn\Desktop\work1.txt", word);
+        }
+    }
+}
