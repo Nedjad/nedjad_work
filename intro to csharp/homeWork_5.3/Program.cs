bc4023480365f111a79bf2adf5af442cda0efa8c
Nedjad <mesh.nedzhad@gmail.com>
2021-03-08 00:28:08 +0300

    Задача 5.3 Ввод числа в файл.

---
+++ intro to csharp/homeWork_5.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace homeWork_5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numb = Console.ReadLine();
+
+            if(int.TryParse(numb, out int result) && result <= 255 && result <= 0)
+            {
+                File.AppendAllText(@"C:\Users\meshn\Desktop\hello.bin", result.ToString());
+            }
+            else
+                Console.WriteLine("Введите число!");
+           
+
+            
+            
+        }
+    }
+}
