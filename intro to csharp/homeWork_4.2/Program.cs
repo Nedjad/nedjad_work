55faf58e2bfb901d65dd8ea82d30b3acc8989292
Nedjad <mesh.nedzhad@gmail.com>
2021-03-07 13:01:58 +0300

    Сложение чисел в строке через цикл for

---
+++ intro to csharp/homeWork_4.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace homeWork_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа в строку!");
+
+            var userStr = Console.ReadLine();
+
+            var strArr = userStr.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var strLeng = userStr.Length;
+
+            int sum = 0;
+
+            for(int i = 0; i < strArr.Length; i++)
+            {
+                if(int.TryParse(strArr[i], out var res))
+                {
+                    sum += res;
+                }
+            }
+            Console.WriteLine(sum);
+
+
+
+
+
+
+        }
+    }
+}
