64884c8f64d604dbcd818adba8ba54041be4b71e
Nedjad <78599846+Nedjad@users.noreply.github.com>
2021-03-23 00:23:02 +0300

    Проект по 8 уроку.

---
+++ intro to csharp/homeWork_8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (string.IsNullOrEmpty(Properties.Settings.Default.userName))
+            {
+
+                Console.WriteLine("Введите имя пользователя:");
+                Properties.Settings.Default.userName = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            
+            if(Properties.Settings.Default.age == 0)
+            {
+                Console.WriteLine("Введите возраст");
+                Properties.Settings.Default.age = int.Parse(Console.ReadLine());
+                Properties.Settings.Default.Save();
+            }
+            
+            if(string.IsNullOrEmpty(Properties.Settings.Default.Profession))
+            {
+                Console.WriteLine("Введите вашу должность:");
+                Properties.Settings.Default.Profession = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            
+            string userName = Properties.Settings.Default.userName;
+            int age = Properties.Settings.Default.age;
+            string Prof = Properties.Settings.Default.Profession;
+            string hello = Properties.Settings.Default.Hello;
+
+            Console.WriteLine($"{hello}, {userName}! Age: {age} Profession: {Prof}!");
+            Console.ReadLine();
+        }
+    }
+}
